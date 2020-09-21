9203e12bf051c80fca405d4f49f55ab950e29c7a
Ryon <52056485+atechnextdoor@users.noreply.github.com>
2020-09-21 17:18:15 -0400

    Update create-users.ps1

--- Office365/create-users.ps1
+++ Office365/create-users.ps1
@@ -2,22 +2,22 @@
 #               Created By: Ryon Riley                #
 #######################################################
 
-Write-Host "Enter 'A' when prompted after the module install starts." -ForegroundColor "Yellow"
-Start-Sleep -s 1
-
 # Set the execution policy
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 
 # Install necessary modules
 Install-PackageProvider -Name NuGet -Force | Out-Null
-Install-Module -Name ExchangeOnlineManagement | Out-Null
-Install-Module MSOnline | Out-Null
+Install-Module -Name ExchangeOnlineManagement -Confirm:$false | Out-Null
+Install-Module -Name MSOnline -Confirm:$false | Out-Null
 
 Write-Host "Enter the Office365 account credentials when prompted..." -ForegroundColor "Yellow"
 Start-Sleep -s 1
 
+# Get O365 admin credentials
+$UserCredential = Get-Credential
+
 # Connect to the O365 service
-Connect-ExchangeOnline -ShowProgress $true | Out-Null
+Connect-ExchangeOnline -Credential $UserCredential
 
 # Enter path to the .csv file
 $CSV = Read-Host "Enter the path to the CSV file"
@@ -25,7 +25,7 @@ $CSV = Read-Host "Enter the path to the CSV file"
 # For each item in the .csv file, create a user account and add to specified groups
 Import-Csv -Path $CSV | foreach {
     try {
-        New-Mailbox -Name $_.UserPrincipalName.Split("@")[0] -FirstName $_.FirstName -LastName $_.LastName -DisplayName $_.DisplayName -MicrosoftOnlineServicesID $_.UserPrincipalName
+        New-Mailbox -Name $_.UserPrincipalName.Split("@")[0] -FirstName $_.FirstName -LastName $_.LastName -DisplayName $_.DisplayName -MicrosoftOnlineServicesID $_.UserPrincipalName | Out-Null
         $groupNames = $_.groupNames -split ","
         foreach($group in $groupNames) {
             Add-DistributionGroupMember -Identity $group -Member $_.UserPrincipalName
@@ -37,5 +37,26 @@ Import-Csv -Path $CSV | foreach {
     }
     Write-Host "All users created successfully." -ForegroundColor "Yellow"
 }
+
+<#
+
+Connect-MsolService -Credential $UserCredential
+
+# Get current licensing information
+$prefix = (Get-MsolAccountSku).AccountSkuId.Split(":")[0]
+$licensing = (Get-MsolAccountSku).AccountSkuId
+
+Import-Csv -Path $CSV | foreach {
+    try {
+        Set-MsolUserLicense -UserPrincipalName $_.UserPrincipalName -AddLicenses $_.AccountSkuId
+    }
+        Write-Host "User" $_.DisplayName "assigned license" $_.AccountSkuId "successfully" -ForegroundColor "Green"
+    catch {
+        "An error occured. Check the .csv file formatting and try again."
+    }
+    Write-Host "All users updated successfully." -ForegroundColor "Yellow"
+}
+
+#>
 
 Disconnect-ExchangeOnline -Confirm:$false | Out-Null
